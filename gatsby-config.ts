45866c1ae5ccc19b0f47568817c6b22ebf1d3dcf
mhaagens <martin.haagensli@gmail.com>
2022-09-16 12:57:54 +0200

    add sitemap plugin

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -3,18 +3,21 @@ import type { GatsbyConfig } from "gatsby";
 const config: GatsbyConfig = {
   siteMetadata: {
     title: `gatsby-demo`,
-    siteUrl: `http://localhost:8000`
+    siteUrl: `https://gatsbydemomain09772.gatsbyjs.io`,
   },
   // More easily incorporate content into your pages through automatic TypeScript type generation and better GraphQL IntelliSense.
   // If you use VSCode you can also use the GraphQL plugin
   // Learn more at: https://gatsby.dev/graphql-typegen
   graphqlTypegen: true,
-  plugins: [{
-    resolve: 'gatsby-plugin-manifest',
-    options: {
-      "icon": "src/images/icon.png"
-    }
-  }]
+  plugins: [
+    {
+      resolve: "gatsby-plugin-manifest",
+      options: {
+        icon: "src/images/icon.png",
+      },
+    },
+    "gatsby-plugin-sitemap",
+  ],
 };
 
 export default config;
--- gatsby-node.js
+++ gatsby-node.js
@@ -2,7 +2,7 @@ exports.createPages = async ({ graphql, actions }) => {
   const { createRedirect } = actions;
 
   createRedirect({
-    fromPath: undefined,
-    toPath: undefined,
+    fromPath: `/test-redirect-2`,
+    toPath: `/page-a`,
   });
 };
